8a1585a36f971aa506c78e60ffe6211f15d83d7e
tsoudibi <47566877+tsoudibi@users.noreply.github.com>
2021-09-15 22:13:23 +0800

    Add files via upload

---
+++ pygame_mediapipe/mediapipe_thread.py
@@ -0,0 +1,132 @@
+# ---------------------------------------------------
+# this file create a thred to do mediapipe in 
+# background, use the result to update mouse or other
+# object.
+# after drawing, save the image as image_main, others
+# can use this to display.
+# result_main is passing to mouse.py to caculate pos
+# ---------------------------------------------------
+
+import cv2
+import mediapipe as mp
+import numpy as np
+import threading
+import time
+
+
+
+# create mediapipe object
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
+mp_hands = mp.solutions.hands
+
+# active camera, use cv2 rather then pygame
+camera = cv2.VideoCapture(0)
+SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, SCREEN_WIDTH)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, SCREEN_HEIGHT)
+
+class mediapipe_data(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self )
+        # dynamic settings of mediapipe thread
+        self.MP_MODE = 'hands'
+        self.MP_LOOP = True
+        self.results_main = None
+        _, image = camera.read()
+        self.image_main = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        self.back_images = []
+        self.back_images.append (cv2.cvtColor(cv2.resize(cv2.imread('.\pygame_mediapipe\images\\fish_tank\\move_0.png', cv2.IMREAD_COLOR),(1280,720),cv2.INTER_LANCZOS4), cv2.COLOR_BGR2RGB))
+        self.back_images.append (cv2.cvtColor(cv2.resize(cv2.imread('.\pygame_mediapipe\images\\fish_tank\\move_1.png', cv2.IMREAD_COLOR),(1280,720),cv2.INTER_LANCZOS4), cv2.COLOR_BGR2RGB))
+        self.back_images.append (cv2.cvtColor(cv2.resize(cv2.imread('.\pygame_mediapipe\images\\fish_tank\\move_2.png', cv2.IMREAD_COLOR),(1280,720),cv2.INTER_LANCZOS4), cv2.COLOR_BGR2RGB))
+
+    def kill_thread(self):
+        self.MP_LOOP = False
+
+
+    def get_image(self):
+        # cv2.IMREAD_UNCHANGED
+        rocks =  cv2.cvtColor(cv2.resize(cv2.imread('.\pygame_mediapipe\images\\fish_tank\\background.png', cv2.IMREAD_COLOR),(1280,720),cv2.INTER_LANCZOS4), cv2.COLOR_BGR2RGB)
+        # add or blend the images
+        image = cv2.addWeighted(self.image_main, 0.1, self.back_images[int(time.time() % 3)], 0.9, 0.0)
+        image = cv2.addWeighted(image, 0.8, rocks, 0.2, 0.0)
+
+        return image
+
+    def get_results(self):
+        if self.results_main:
+            return self.results_main
+        else:
+            return None
+    
+    # funtion to run mediapipe and create image 
+    def run(self):
+        global camera
+        if self.MP_MODE == 'pose':
+            pose = mp_pose.Pose(min_detection_confidence = 0.5, min_tracking_confidence = 0.5)
+        elif self.MP_MODE == 'hands':
+            hands = mp_hands.Hands(min_detection_confidence = 0.8, min_tracking_confidence = 0.5)
+        while  camera.isOpened() and self.MP_LOOP:
+            success, image = camera.read()
+            # image = fish_eye_fix(image)
+            if not success:
+                print("Ignoring empty camera frame.")
+                # video capture fail, use backup image
+                self.image_main = self.image_backup
+
+            # Flip the image horizontally for a later selfie-view display, and convert
+            # the BGR image to RGB.
+            image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+            # To improve performance, optionally mark the image as not writeable to
+            # pass by reference.
+            image.flags.writeable = False
+            if self.MP_MODE == 'pose':
+                results = pose.process(image)
+                self.results_main = results 
+            elif self.MP_MODE == 'hands':
+                results = hands.process(image)
+                self.results_main = results 
+            # Draw the pose annotation on the image.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            # drawing landmarks
+            if self.MP_MODE == 'pose':
+                mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+            elif self.MP_MODE == 'hands':
+                # display mouse
+                #image = mouse_1.display(image, results)
+                #image = mouse_2.display(image, results)
+                if results.multi_hand_landmarks:
+                    for hand_landmarks in results.multi_hand_landmarks:
+                        mp_drawing.draw_landmarks(image,hand_landmarks,mp_hands.HAND_CONNECTIONS)
+            # after drawing, change color back tp rgb
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            # save image to global and backup
+            self.image_main = image
+            self.image_backup = image
+        # if anything wrong happend, use backup image
+        self.image_main = self.image_backup
+        # https://google.github.io/mediapipe/solutions/pose#static_image_mode
+
+# fix matrix to correct fisheye
+DIM = (2560, 1440)
+K = np.array([[1694.7801205342007, 0.0, 1363.235424743914], [0.0, 1694.4106664109822, 686.0493220616949], [0.0, 0.0, 1.0]])
+D = np.array([[-0.010547721322285927], [0.02576312669611254], [-0.1546810103404629], [0.15853057545495458]])
+
+def fish_eye_fix(image):
+    # this program is quoted from others, see more information in fisheye_fix.py
+    dim1 = image.shape[:2][::-1]  #dim1 is the dimension of input image to un-distort
+    assert dim1[0]/dim1[1] == DIM[0]/DIM[1], "Image to undistort needs to have same aspect ratio as the ones used in calibration"
+    image = cv2.resize(image,DIM,interpolation=cv2.INTER_AREA)
+    Knew = K.copy()
+    Knew[(0,1), (0,1)] = 0.6 * Knew[(0,1), (0,1)] #scaling
+    map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), Knew, DIM, cv2.CV_16SC2)
+    undistorted_img = cv2.remap(image, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+    x,y,w,h = 475,254,1536,864
+    img_valid = undistorted_img[y:y+h, x:x+w]
+    img_valid = cv2.resize(img_valid, (1280, 720), interpolation=cv2.INTER_AREA)
+    return img_valid
+    # I finally dont use this because it takes 0.12s per frame in averge, too havy
+
---
+++ pygame_mediapipe/sprites.py
@@ -0,0 +1,179 @@
+import pygame
+import random
+import math 
+import time
+
+# import other files 
+import mediapipe_thread as mp_thread
+import spritesheet 
+
+SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
+
+class ball(pygame.sprite.Sprite):
+    def __init__(self, radius, x, y, color) :
+        # init by parent
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.Surface([radius*2, radius*2])  # create canvas
+        self.image.set_colorkey((0,0,0))
+        self.radius = radius # set radius for circle sprite (for collide_circle)
+        self.rect = self.image.get_rect() # create rect for collide detection 
+        self.rect.center = (x, y)  # set initial position
+        self.color = color
+        self.forced = False # weather the ball is forced to move
+
+        pygame.draw.circle(self.image, color, [radius,radius], radius, 0)
+
+        self.kill = False
+
+    def update(self):
+        self.rect.y += 2
+        self.rect.x += random.randint(-5,5)
+        if self.rect.y >=  SCREEN_HEIGHT:
+            self.kill = True
+
+class mouse(pygame.sprite.Sprite):
+    def __init__(self, idx, radius, x, y, color) :
+        # init by parent
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.Surface([radius*2, radius*2])  # create canvas
+        self.image.set_colorkey((0,0,0))
+        self.radius = radius # set radius for circle sprite (for collide_circle)
+        self.rect = self.image.get_rect() # create rect for collide detection 
+        self.rect.center = (x, y)  # set initial position
+        self.color = color
+
+        pygame.draw.circle(self.image, color, [radius,radius], radius, 0)
+
+        self.kill = False
+        self.idx = idx
+        self.distence_tip = 0
+        self.distence_mcp = 0
+
+    def kill(self):
+        self.kill = True
+    
+    def is_push_down(self):
+        return self.distence_tip < self.distence_mcp
+
+    def update(self, results):
+        # make sure there is hand detected and weather each landmarks exist
+        if results != None:
+            if results.multi_hand_landmarks :
+                for hands_idx, hand_landmarks in enumerate(results.multi_hand_landmarks):
+                    if hand_landmarks:
+                        for idx, landmark in enumerate(hand_landmarks.landmark):
+                            if idx == 2: # THUMB_MCP
+                                THUMB_MCP_x, THUMB_MCP_y = int(landmark.x * SCREEN_WIDTH), int(landmark.y * SCREEN_HEIGHT)
+                            elif idx == 4: # THUMB_TIP
+                                THUMB_TIP_x, THUMB_TIP_y = int(landmark.x * SCREEN_WIDTH), int(landmark.y * SCREEN_HEIGHT)
+                            elif idx == 5: # INDEX_FINGER_MCP
+                                INDEX_FINGER_MCP_x, INDEX_FINGER_MCP_y = int(landmark.x * SCREEN_WIDTH), int(landmark.y * SCREEN_HEIGHT)
+                            elif idx == 8: # INDEX_FINGER_TIP
+                                INDEX_FINGER_TIP_x, INDEX_FINGER_TIP_y = int(landmark.x * SCREEN_WIDTH), int(landmark.y * SCREEN_HEIGHT)
+                        if hands_idx == self.idx :
+                            self.distence_tip = math.isqrt((THUMB_TIP_x - INDEX_FINGER_TIP_x)**2 + (THUMB_TIP_y - INDEX_FINGER_TIP_y)**2)
+                            self.distence_mcp = math.isqrt((THUMB_MCP_x - INDEX_FINGER_MCP_x)**2 + (THUMB_MCP_y - INDEX_FINGER_MCP_y)**2)
+                            # redraw the circle
+                            radius = int(self.distence_tip * 0.15) #change size
+                            self.image = pygame.Surface([radius*2, radius*2])  # create canvas
+                            self.image.set_colorkey((0,0,0))
+                            self.radius = radius # set radius for circle sprite (for collide_circle)
+                            self.rect = self.image.get_rect() # create rect for collide detection 
+                            self.rect.center = (round((THUMB_TIP_x + INDEX_FINGER_TIP_x)/2-radius/2), round((THUMB_TIP_y + INDEX_FINGER_TIP_y)/2-radius/2))
+
+                            # test
+                            if self.is_push_down():
+                                self.color = (0,255,0)
+                            else:
+                                self.color = (0,0,255)
+                            pygame.draw.circle(self.image, self.color, [radius,radius], radius, 0)
+
+
+class fish(pygame.sprite.Sprite):
+    def __init__(self, x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2, color = (0,0,0),
+                type = 'doux', speed = 3, scale = 10) :
+        # init by parent
+        pygame.sprite.Sprite.__init__(self)
+
+        ## sprite sheet
+        if type == 'doux':
+            dino_ss = spritesheet.spritesheet('.\pygame_mediapipe\images\dino_sheets\Dino_doux.png')
+        elif type == 'mort':
+            dino_ss = spritesheet.spritesheet('.\pygame_mediapipe\images\dino_sheets\Dino_mort.png')
+        elif type == 'tard':
+            dino_ss = spritesheet.spritesheet('.\pygame_mediapipe\images\dino_sheets\Dino_tard.png')
+        elif type == 'vita':
+            dino_ss = spritesheet.spritesheet('.\pygame_mediapipe\images\dino_sheets\Dino_vita.png')
+        self.idle_frame_R = []
+        self.move_frame_R = []
+        self.idle_frame_R = dino_ss.images_at( ((4, 4, 15, 17), (28, 4, 15, 17),(52, 4, 15, 17),(76, 4, 15, 17)), scale = scale)
+        self.move_frame_R = dino_ss.images_at( ((76, 4, 15, 17), (100, 4, 15, 17),(124, 4, 15, 17),(148, 4, 15, 17),
+                                            (172, 4, 15, 17),(196, 4, 15, 17),(220, 4, 15, 17),(244, 4, 15, 17)), scale = scale)
+        self.idle_frame_L = spritesheet.flip_images(self.idle_frame_R)
+        self.move_frame_L = spritesheet.flip_images(self.move_frame_R)
+
+        self.image = self.move_frame_R[0]  # create canvas
+        self.image.set_colorkey((0,0,0))
+        self.rect = self.image.get_rect() # create rect for collide detection 
+        self.rect.center = (x, y)  # set initial position
+        self.kill = False
+
+        #pygame.draw.circle(self.image, color, [radius,radius], radius, 0)
+
+        # fish setting 
+        self.color = color
+        self.speed = speed
+        self.forced = False # weather the fish is forced to move
+        self.dir = (random.randint(self.speed * -1,self.speed),random.randint(self.speed * -1,self.speed)) # moving direction \
+        self.excite = 200 # the smaller, the fasterthe fish will change its dir
+        self.excite_timer = self.excite
+        self.facing = 'R'
+    
+    def draw(self, screen):
+        pygame.draw.rect(screen, (255,0,0), self.rect, 2)
+
+    def update(self, mouse):
+        if  pygame.sprite.collide_rect(self, mouse) and mouse.is_push_down():
+            self.forced = True
+            # forced to move
+            self.rect.y = mouse.rect.y - self.rect.height / 2
+            self.rect.x = mouse.rect.x - self.rect.width / 2
+        else:
+            self.forced = False
+            # free swimming
+            if self.excite_timer <= 0:
+                # out of excite, change dir
+                self.dir = (random.randint(self.speed * -1,self.speed),random.randint(self.speed * -1,self.speed))
+                self.excite_timer = self.excite
+            else:
+                # set the range of the fish to move
+                border = 100
+                # horizontal
+                if self.rect.y <=  SCREEN_HEIGHT - self.rect.height - border and self.rect.y >=  border:
+                    self.rect.y += self.dir[1]
+                else :
+                    # if out of border, change direction 
+                    if self.rect.y > SCREEN_HEIGHT/2 : self.rect.y = SCREEN_HEIGHT - self.rect.height - border
+                    else: self.rect.y =  border
+                    self.dir =  (self.dir[0], self.dir[1] * -1)
+                # vertical
+                if self.rect.x <=  SCREEN_WIDTH - self.rect.width - border and self.rect.x >=  border:
+                    self.rect.x += self.dir[0]
+                else:
+                    # if out of border, change direction 
+                    if self.rect.x > SCREEN_WIDTH/2 : self.rect.x =  SCREEN_WIDTH - self.rect.width - border
+                    else: self.rect.x =  border
+                    self.dir =  (self.dir[0] * -1, self.dir[1])
+            # changing frame by facing
+            if self.dir[0] > 0:
+                self.facing = 'R'
+                frame = (self.rect.x // 30) % len(self.move_frame_R)
+                self.image = self.move_frame_R[frame]
+            elif self.dir[0] < 0:
+                self.facing = 'L'
+                frame = (self.rect.x // 30) % len(self.move_frame_L)
+                self.image = self.move_frame_L[frame]
+            self.excite_timer -= 1
+
